0d0308c1f9b3ce367a63fac1179a10064b748cde
Brandon Pouliot <brandonpouliot@gmail.com>
2024-05-30 19:15:38 -0400

    added get user controller method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,3 +6,14 @@ exports.createUser = (req, res) => {
         .then(() => res.status(201).send('User created successfully'))
         .catch(err => res.status(500).send('Error creating user'));
 };
+
+exports.getUser = (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).send('User not found');
+            }
+            res.status(200).json(user);
+        })
+        .catch(err => res.status(500).send('Error retrieving user'));
+};
