5f3ebe2a5e97a4dc986c04159ad110652cd8ba5d
Brandon Pouliot <brandonpouliot@gmail.com>
2024-05-30 21:27:00 -0400

    added function to update job posting

--- server/controllers/employer.js
+++ server/controllers/employer.js
@@ -25,3 +25,16 @@ exports.getJobs = async (req, res) => {
         res.status(500).json({ message: 'Failed to retrieve jobs', error });
     }
 };
+
+exports.updateJob = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true });
+        if (!updatedJob) {
+            return res.status(404).json({ message: 'Job not found' });
+        }
+        res.status(200).json(updatedJob);
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to update job', error });
+    }
+};
