940bee8fd7447f989c6e2c8f5dc722ce1545a3b4
Brandon Pouliot <brandonpouliot@gmail.com>
2024-06-03 19:07:19 -0400

    added get single client by id

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -19,3 +19,15 @@ exports.getAllClients = async (req, res) => {
         res.status(500).json({ message: 'Error retrieving clients', error });
     }
 };
+
+exports.getClient = async (req, res) => {
+    try {
+        const client = await Client.findById(req.params.id);
+        if (!client) {
+            return res.status(404).json({ message: 'Client not found' });
+        }
+        res.status(200).json(client);
+    } catch (error) {
+        res.status(500).json({ message: 'Error retrieving client', error });
+    }
+};
