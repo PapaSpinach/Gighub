51893c9989c2a718ba4193a68a69c8ebbdcaa788
Brandon Pouliot <brandonpouliot@gmail.com>
2024-06-03 19:06:11 -0400

    added get all clients controller method

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -10,3 +10,12 @@ exports.createClient = async (req, res) => {
         res.status(500).json({ message: 'Error creating client', error });
     }
 };
+
+exports.getAllClients = async (req, res) => {
+    try {
+        const clients = await Client.find();
+        res.status(200).json(clients);
+    } catch (error) {
+        res.status(500).json({ message: 'Error retrieving clients', error });
+    }
+};
