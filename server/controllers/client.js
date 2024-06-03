9ca37ce404b1cdb7721a45bdb62a7283dad036b0
Brandon Pouliot <brandonpouliot@gmail.com>
2024-06-03 19:05:29 -0400

    added create client controller method

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,2 +1,12 @@
 const Client = require('server/models/clients')
 
+exports.createClient = async (req, res) => {
+    try {
+        const { fullName, userName, password, email, phoneNumber } = req.body;
+        const newClient = new Client({ fullName, userName, password, email, phoneNumber });
+        await newClient.save();
+        res.status(201).json({ message: 'Client created successfully', data: newClient });
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating client', error });
+    }
+};
