02392c3727424708b59dabe90cadca8acbc20b2b
Brandon Pouliot <brandonpouliot@gmail.com>
2024-06-05 19:35:59 -0400

    added middleware to protect routes

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -46,3 +46,22 @@ exports.login = async (req, res) => {
         res.status(500).json({ message: 'Error logging in', error });
     }
 };
+
+exports.protect = async (req, res, next) => {
+    try {
+        let token;
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if (!token) {
+            return res.status(401).json({ message: 'Not authorized to access this route' });
+        }
+
+        const decoded = jwt.verify(token, 'your_jwt_secret');
+        req.user = await User.findById(decoded.userId).select('-password');
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Not authorized', error });
+    }
+};
