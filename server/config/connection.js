4d9d736549a672a5adb41ab530e07a91990f3ed5
KevinKiely <Kevinkiely762@gmail.com>
2024-06-10 18:23:08 -0400

    Final Touches

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,15 @@
-const { connect, connection } = require('mongoose');
+// const { connect, connection } = require('mongoose');
 
+/*
 const connectionString = 'mongodb://127.0.0.1:27017/gighubDB';
 
 connect(connectionString).then(console.log('Connected to Database'));
 
 module.exports = connection;
+*/
 
-
-
-
-
-/* ALTERNATIVE CONNECTION METHOD
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gighubDB');
+ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gighubDB');
 
 module.exports = mongoose.connection;
-*/
