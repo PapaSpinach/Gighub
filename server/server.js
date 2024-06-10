d3bb01d42ef73cd3591f52173b2ba649f3cff7e7
KevinKiely <Kevinkiely762@gmail.com>
2024-06-10 18:52:11 -0400

    server.js

--- server/server.js
+++ server/server.js
@@ -6,14 +6,25 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(routes);
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
+
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+
