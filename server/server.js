6fcbf5c3c3c5c0fcc8209f1da23ebde8b3f9f7c2
Aaron Beckles <therealini@gmail.com>
2024-06-12 17:04:58 -0400

    edit line 5

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
