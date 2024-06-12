7b0c26915356944585a28bcc612ac9f9e902abfb
Aaron Beckles <therealini@gmail.com>
2024-06-12 16:46:51 -0400

    update api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:3001/api';
+export const API_URL = '/api';
 
 export async function fetchAPI(url, options) {
   const authToken = localStorage.getItem('token');
